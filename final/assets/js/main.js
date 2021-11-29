863aacba46aae9b260e54618bc4efe3b3b3df3ae
mengfw-02 <63938598+mengfw-02@users.noreply.github.com>
2021-11-29 16:20:30 -0500

    draft 32

--- final/assets/js/main.js
+++ final/assets/js/main.js
@@ -1,6 +1,49 @@
 // var habiter = {
 //   "habit": "empty",
-//   "days": 0
+//   "days": 0,
+//   "counter": 0
 // };
+//
+$("#main").load("test.html");
+// console.log("hello");
 
-$("#main").load('views/about.html');
+var counter = 0;
+var habit = "empty";
+var days = 0;
+$(document).ready(function(){
+  // $("form").on('submit',function(event) {
+  //   input = document.getElementById("habitInput").value;
+  //   console.log(input);
+  $("#amazon").hide();
+  $("#progress").hide();
+
+});
+if(document.getElementById("amazonLink") != null){
+  document.getElementById("amazonLink").href += habit;
+}
+
+$( "#yes" ).click(function() {
+  counter++;
+  console.log(counter);
+  $("#yes").hide();
+  $("#no").hide();
+  $("#amazon").show();
+});
+
+$( "#no" ).click(function() {
+  console.log(counter);
+  $("#yes").hide();
+  $("#no").hide();
+  $("#amazon").show();
+});
+
+$( "#amazonLink" ).click(function() {
+  console.log(counter);
+  $("#nothx").hide();
+  $("#progress").show();
+});
+$( "#nothx" ).click(function() {
+  console.log(counter);
+  $("#nothx").hide();
+  $("#progress").show();
+});
